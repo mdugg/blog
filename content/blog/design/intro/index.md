6dd2bc265531bd6eae019748bc7954b62b07b8b2
Martin Duggan <martinduggan@Martins-MBP.attlocal.net>
2020-11-19 12:53:07 -0500

    initial push of Gatsby boilerplate

--- content/blog/design/intro/index.md
+++ content/blog/design/intro/index.md
@@ -1,7 +1,7 @@
 ---
-title: Hello World
+title: Intro to design
 date: "2015-05-01T22:12:03.284Z"
-description: "Hello World"
+description: "Intro to design"
 ---
 
 This is my first post on my new fake blog! How exciting!
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,24 @@
+// git init
+// git clone git@github.com:mdugg/blog.git
+// git status
+// git add .
+// git commit -m "initial push of Gatsby boilerplate"
+// git push
+
+// npm install --save gatsby-plugin-sass
+// npm install node-sass gatsby-plugin-sass
+// Error: Node Sass version 5.0.0 is incompatible with ^4.0.0
+// https://stackoverflow.com/questions/64625050/error-node-sass-version-5-0-0-is-incompatible-with-4-0-0
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/assets/normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/assets/style.css"
+import "./src/assets/app.scss"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-okaidia.css"
